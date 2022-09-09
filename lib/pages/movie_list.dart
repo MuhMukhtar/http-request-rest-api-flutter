22816871b4f57205e47b52c29738f90e1440f061
MuhMukhtar <mukhtar55217@gmail.com>
2022-09-09 09:21:46 +0800

    listview

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -1,24 +1,52 @@
 import 'package:flutter/material.dart';
-import 'package:movie_catalog/service/http_service.dart';
+import 'package:mov/service/http_service.dart';
 
-class MovieList extends StatefulWidget {
+class Movielist extends StatefulWidget {
   @override
-  State<MovieList> createState() => _MovieListState();
+  _MovielistState createState() => _MovielistState();
 }
 
-class _MovieListState extends State<MovieList> {
+class _MovielistState extends State<Movielist> {
+  int? moviesCount;
+  List? movies;
   String result = "";
-  late HttpService service;
+  HttpService? service;
+
+  Future initialize() async {
+    movies = [];
+    movies = await service!.getPopularMovies();
+    setState(() {
+      moviesCount = movies!.length;
+      movies = movies;
+    });
+  }
 
   @override
-  void initState(){
+  void initState() {
     service = HttpService();
+    initialize();
     super.initState();
   }
-  
-  @override
-  Widget build(BuildContext context){
-    return Container();
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Popular Movies"),
+        ),
+        body: ListView.builder(
+          itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
+          itemBuilder: (context, int position) {
+            return Card(
+              color: Colors.white,
+              elevation: 2.0,
+              child: ListTile(
+                title: Text(movies![position].title),
+                subtitle: Text(
+                  'Rating = ' + movies![position].voteAverage.toString(),
+                ),
+              ),
+            );
+          },
+        ));
   }
-  
 }
--- lib/service/http_service.dart
+++ lib/service/http_service.dart
@@ -6,18 +6,22 @@ import 'package:movie_catalog/models/movie.dart';
 class HttpService {
   final String apiKey = '1bffa81aca7bd449077fb8f6d25ff75f';
   final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+  
+  get moviesMap => null;
 
-  Future<String?> getPopularMovies() async {
+  Future<List?> getPopularMovies() async {
     final String uri = baseUrl + apiKey;
 
     http.Response result = await http.get(Uri.parse(uri));
     if (result.statusCode == HttpStatus.ok) {
       print("Sukses");
-      String response = result.body;
-      return response;
+      final jsonResponse = json.decode(result.body);
+      final moviesMap = jsonResponse['results'];
+      List movies = moviesMap.map((i) => Movie.fromJson(i)).toList();
+      return movies;
     } else {
       print("Fail");
-      return null;
+      return Future.value();
     }
   }
 }
